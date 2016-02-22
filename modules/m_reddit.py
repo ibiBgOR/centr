f003b3acfd59fe2ac1e779d6d84c6bcc3772c2ba
Ruckdeschel Oscar <oscar.ruckdeschel@data-experts.de>
2016-02-22 15:31:24 +0100

    Retrived the image from reddit posts

--- modules/m_reddit.py
+++ modules/m_reddit.py
@@ -14,6 +14,12 @@ SUBREDDIT = 'r'
 
 reddit_feeds = []
 
+user_post_source_map = {
+    'submission': 'User {0} has posted on subreddit {1}',
+    'comment': 'User {0} commented on a post'
+}
+subreddit_action = 'New post in subreddit {0}'
+
 conn = DBConnection()
 reddit_conn = praw.Reddit(user_agent = 'centr')
 
@@ -36,87 +42,112 @@ def get_feeds():
 
     count = 0
     for feed in reddit_feeds:
-        if feed.source == USERS:
-            src = 'User {0} commented on a post'.format(feed.name)
-        elif feed.source == SUBREDDIT:
-            src = 'New post in subreddit {0}'.format(feed.name)
-        else:
-            continue
-
-        feedsources.append({'src': src, 'counter': 0, 'max_count': feed.max_count})
-        feed_map[src] = count
+        feedsources.append({'src': feed.name, 'counter': 0, 'max_count': feed.max_count})
+        feed_map[feed.name] = count
         count += 1
 
     feeds = conn.get_feeds(TYPE)
 
     for feed in feeds:
 
-        if feed.source in [elem['src'] for elem in feedsources]:
-            max_count = feedsources[feed_map[feed.source]]['max_count']
+        loaded_source = json.loads(feed.source)
+
+        if loaded_source['title'] in [elem['src'] for elem in feedsources]:
+            max_count = feedsources[feed_map[loaded_source['title']]]['max_count']
         else:
             max_count = 0
 
         try:
-            if max_count != -1 and feedsources[feed_map[feed.source]]['counter'] >= max_count:
+            if max_count != -1 and feedsources[feed_map[loaded_source['title']]]['counter'] >= max_count:
                 continue
         except KeyError, e:
             # if the feed source is no longer available in the config
             continue
 
+        if 'source' in loaded_source:
+            # We get a user (is it a comment or a post?)
+            if loaded_source['source'] == 'comment':
+                source = user_post_source_map[loaded_source['source']].format(loaded_source['title'])
+            elif loaded_source['source'] == 'submission':
+                source = user_post_source_map[loaded_source['source']].format(loaded_source['title'], loaded_source['subreddit'])
+        else:
+            source = subreddit_action.format(loaded_source['title'])
+
+
         result.append(Feeditem(
             json.loads(feed.content),
             TYPE,
-            feed.source,
+            source,
             feed.time
         ))
-        feedsources[feed_map[feed.source]]['counter'] += 1
+        feedsources[feed_map[loaded_source['title']]]['counter'] += 1
 
     return result
 
 def _load_feeds():
     for element in reddit_feeds:
         if element.source == SUBREDDIT:
-            #try:
-            posts = reddit_conn.get_subreddit(element.name).get_new()
-            for post in posts:
-                conn.insert_element(DBFeedItem(
-                    json.dumps({'content': post.title}), # content
-                    TYPE, # type
-                    'New post in subreddit {0}'.format(element.name), # source
-                    _get_date(post), # time
-                ))
-            #except ConnectionError, e:
-            #    print str(e)
+            try:
+                posts = reddit_conn.get_subreddit(element.name).get_new()
+                for post in posts:
+                    content = {'post_name': post.title}
+
+                    if hasattr(post, 'preview'):
+                        img = {'source': '', 'width': 0}
+                        preview = post.preview
+                        for image in preview['images'][0]['resolutions']:
+                            if image['width'] > 640:
+                                continue
+                            if image['width'] > img['width']:
+                                img['source'] = image['url']
+                                img['width'] = image['width']
+                        content['thumbnail'] = img['source']
+
+                    conn.insert_element(DBFeedItem(
+                        json.dumps(content), # content
+                        TYPE, # type
+                        json.dumps({'title': element.name}), # source
+                        _get_date(post), # time
+                    ))
+            except ConnectionError, e:
+                print str(e)
 
         elif element.source == USERS:
-            #try:
-            redditor = reddit_conn.get_redditor(element.name)
-            for comment in redditor.get_overview(sort = 'new', time = 'all'):
-                print dir(comment)
+            try:
+                redditor = reddit_conn.get_redditor(element.name)
+                for comment in redditor.get_overview(sort = 'new', time = 'all'):
+                    if isinstance(comment, praw.objects.Comment):
+                        source = 'comment'
+                    elif isinstance(comment, praw.objects.Submission):
+                        source = 'submission'
+                    else:
+                        continue
 
-                content = {}
+                    content = {}
 
-                if hasattr(comment, 'body_html'):
-                    content['content'] = comment.body_html
+                    if hasattr(comment, 'body'):
+                        content['content'] = comment.body
 
-                if hasattr(comment, 'selftext_html'):
-                    content['post_name'] = comment.selftext_html
-                elif hasattr(comment, 'link_title'):
-                    content['post_name'] = comment.link_title
-                else:
-                    print dir(comment)
-                    continue
+                    subreddit = ''
+                    if hasattr(comment, 'subreddit'):
+                        subreddit = comment.subreddit.name
 
-                conn.insert_element(DBFeedItem(
-                    json.dumps(content), # content
-                    TYPE, # type
-                    'User {0} commented on a post'.format(element.name), # source
-                    _get_date(post), # time
-                ))
-            #except Exception, e:#ConnectionError, e:
-            #    print str(e)
+                    if hasattr(comment, 'selftext'):
+                        content['post_name'] = comment.selftext
+                    elif hasattr(comment, 'link_title'):
+                        content['post_name'] = comment.link_title
+                    else:
+                        continue
 
-            # print reddit_conn.get_redditor(user).get_submitted('new', 'all').get_content()
+                    conn.insert_element(DBFeedItem(
+                        json.dumps(content), # content
+                        TYPE, # type
+                        json.dumps({'title': element.name, 'source': source, 'subreddit': subreddit}), # source
+                        _get_date(post), # time
+                    ))
+
+            except ConnectionError, e:
+                print str(e)
 
 def reload_config():
     if 'subreddits' in config:
