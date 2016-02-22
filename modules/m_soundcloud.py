99138f09ba379323a3f9c868862513a1204f4a1d
Ruckdeschel Oscar <oscar.ruckdeschel@data-experts.de>
2016-02-22 15:31:01 +0100

    ConnectionError on Soundcloud module catched

--- modules/m_soundcloud.py
+++ modules/m_soundcloud.py
@@ -1,6 +1,7 @@
 import soundcloud
 import datetime
 import json
+from requests.exceptions import ConnectionError
 from config import soundcloud as config
 from db import DBConnection, DBFeedItem
 from feeditem import Feeditem
@@ -54,36 +55,37 @@ def _load_feeds():
 
     try:
         elements = client.get('/me/activities').collection
+        for element in elements:
+            _type = element.type
+            if _type not in accepted_types:
+                continue
+
+            source = source_type_mapping[_type]
+
+            if _type == 'track' or _type == 'track-repost':
+                content = json.dumps({
+                    'id': element.origin.id,
+                    'username': element.origin.user['username'],
+                    'tracktitle': element.origin.title,
+                    'description': element.origin.description,
+                    'songurl': element.origin.permalink_url,
+                    'artworkurl': element.origin.artwork_url.replace('large', 't300x300'),
+                    'waveformurl': element.origin.waveform_url,
+                    'genre': element.origin.genre,
+                    'duration': element.origin.duration,
+                })
+                source = source.format(element.origin.user['username'])
+            elif _type == 'comment':
+                pass
+            elif _type == 'favoriting':
+                pass
+            else:
+                print 'Not accepted!'
+                continue
+    except ConnectionError, e:
+        return 
     except Exception, e:
         return
-
-    for element in elements:
-        _type = element.type
-        if _type not in accepted_types:
-            continue
-
-        source = source_type_mapping[_type]
-
-        if _type == 'track' or _type == 'track-repost':
-            content = json.dumps({
-                'id': element.origin.id,
-                'username': element.origin.user['username'],
-                'tracktitle': element.origin.title,
-                'description': element.origin.description,
-                'songurl': element.origin.permalink_url,
-                'artworkurl': element.origin.artwork_url.replace('large', 't300x300'),
-                'waveformurl': element.origin.waveform_url,
-                'genre': element.origin.genre,
-                'duration': element.origin.duration,
-            })
-            source = source.format(element.origin.user['username'])
-        elif _type == 'comment':
-            pass
-        elif _type == 'favoriting':
-            pass
-        else:
-            print 'Not accepted!'
-            continue
 
         time = datetime.datetime.strptime(element.created_at, '%Y/%m/%d %H:%M:%S +%f') # e.g. 2016/02/17 15:03:31 +0000
 
